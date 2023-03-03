e714e4e83efbac6d99d651608b1a82957bd08875
Jane Luz <janerodrigues_2610@hotmail.com>
2023-03-03 13:45:05 -0300

    chore: add rotas get

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,30 @@ import { randomUUID } from 'crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where({ id }).first()
+    return { transaction }
+  })
+
+  // passar o retorno/ envio como objeto facilita pra qnd quiser inserir novas informações, não atrapalhar o resultado que já tinha
+
+  app.get('/summary', async () => {
+    const transactions = await knex('transactions')
+      .sum('amount as amount') // o as amount é para colocar o nome na coluna
+      .first()
+    return { transactions }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
