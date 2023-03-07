ede84662752d91d5903995a93bf0046281c6de5f
Jane Luz <janerodrigues_2610@hotmail.com>
2023-03-07 14:06:37 -0300

    feat: add test

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,7 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
-
-const app = fastify({ logger: true })
-
-app.register(cookie)
-
-// app.addHook('preHandler', async (request, reply) => {
-//   console.log(`'${request.method} ${request.url}'`)
-// })
-
-app.register(transactionsRoutes, {
-  prefix: '/transactions',
-})
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP Server Running!')
 })
+export { app }
--- test/exemple.spec.ts
+++ test/exemple.spec.ts
@@ -1,6 +1,22 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/server'
 
-test('Ousuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201
-  expect(responseStatusCode).toEqual(201)
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('User can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'new transaction',
+      amount: 5000,
+      description: 'Salário',
+    })
+    .expect(201)
 })
